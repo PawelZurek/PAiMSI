3e9d54ce1c53088fed74297dbb881545ab0edf6e
PawelZurek <200404@student.pwr.edu.pl>
2014-05-22 11:03:47 +0200

    Simplex
    male poprawki

--- simplex/prj/src/main.cpp
+++ simplex/prj/src/main.cpp
@@ -17,6 +17,5 @@ int main(){
 	s.wyswietl();
 	s.oblicz();
 
-
 	return 0;
 }
--- simplex/prj/src/simplex.cpp
+++ simplex/prj/src/simplex.cpp
@@ -75,6 +75,8 @@ void simplex::wyswietl(){
 
 void simplex::oblicz(){
 	int m=0;
+	int indeks=0;
+	double zysk_max = 0, x_max = 0, y_max = 0 ,x_tmp= 0, y_ma=0,y_tmp=0;
 	x[0]=9999,y[0]=9999;
 
 	for (int i = 0; i < n; i++)
@@ -84,25 +86,28 @@ void simplex::oblicz(){
 	for (int i = 0; i < n; i++)
 		if ((tab[i][0] != 0) || (tab[i][1] != 0)){
 			if ((tab[i][0] != 0) && (ograniczenia[i] < x[0]))
-				x[0] = ograniczenia[i] / tab[i][0];//prosta
+				x[0] = ograniczenia[i] / tab[i][0];//obliczenie punktu prostej x
 			if ((tab[i][1] != 0) && (ograniczenia[i] < y[0]))
-				y[0] = ograniczenia[i] / tab[i][1];
+				y[0] = ograniczenia[i] / tab[i][1];//obliczenie punktu prostej y
 		}
-	x[1] = 0;//0
-	y[1] = 0;
+	x[1] = 0;//punkt 0 dla x
+	y[1] = 0;//punkt 0 dla y
 	if ((ograniczenia[m] - (y[0]*tab[m][1]))/tab[m][0] < 0)
 		x[2] = 0;
-	else
+	else//obliczenie punkty przeciecia prostej z linia dla x
 		x[2] = (ograniczenia[m] - (y[0]*tab[m][1]))/tab[m][0];//linia
 	if ((ograniczenia[m] - (x[0]*tab[m][0]))/tab[m][1] <0)
 		y[2] = 0;
-	else
+	else//obliczenie punkty przeciecia prostej z linia dla y
 		y[2] = (ograniczenia[m] - (x[0]*tab[m][0]))/tab[m][1];
 	
-	x[3] = ograniczenia[m]/tab[m][0];
-	y[3] = ograniczenia[m]/tab[m][1];
+	x[3] = ograniczenia[m]/tab[m][0];//obliczenie punktu przecieca lini z OSIa dla x
+	y[3] = ograniczenia[m]/tab[m][1];//obliczenie punktu przecieca lini z OSIa dla y
 
-	if (y[0] < y[3])
+	for (int i = 0; i < 8; i++)//wyzerowanie zyskow
+		zysk[i] = 0;
+
+	if (y[0] < y[3])//sprawdzenie czy punkty sa w granicach 
 		zysk[0] = (x[1]*koszt[0]) + (y[0]*koszt[1]);
 	else
 		zysk[1] = (x[1]*koszt[0]) + (y[3]*koszt[1]);
@@ -121,15 +126,28 @@ void simplex::oblicz(){
 			zysk[6] = (x[2]*koszt[0]) + (y[0]*koszt[1]);
 	}
 	}
-	int indeks=0;
-	double zysk_max = 0, x_max = 0, y_max = 0 ;
+	for (double i = 0; i < x[3]; i=i+0.1){//najlepszy punkt na prostej
+		y_tmp = (ograniczenia[m] - (i*tab[m][0]))/tab[m][1];
+		if ((i < x[0]) && ( y_tmp < y[0]) )
+			if (((i*koszt[0]) + ((y_tmp)*koszt[1]))>zysk[7]){
+			zysk[7]=(i*koszt[0]) + ((y_tmp)*koszt[1]);
+			cout<<zysk[7]<<endl;
+			x_tmp = i;
+			y_ma = y_tmp; 
+		}
+	}
 
-	for (int i = 0; i < 7; i++){
+	for (int i = 0; i < 8; i++){
 		if (zysk[i] > zysk_max){
 			zysk_max = zysk[i];
 			indeks = i;
 		}
+		cout<<zysk[i]<<endl;
 	}
+
+
+
+
 	switch(indeks){
 		case 0: x_max = x[1] ; y_max = y[0]; break;
 		case 1: x_max = x[1] ; y_max = y[3]; break;
@@ -138,6 +156,7 @@ void simplex::oblicz(){
 		case 4: x_max = x[0] ; y_max = y[0]; break;
 		case 5: x_max = x[0] ; y_max = y[2]; break;
 		case 6: x_max = x[2] ; y_max = y[0]; break;
+		case 7: x_max = x_tmp ; y_max = y_ma; break;
 		default : cout<<"blad"; break;
 
 	}
